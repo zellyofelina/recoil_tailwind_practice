2cb683eaa5907e0f75c9ea0affad073d3dceaba1
elina <elina@elinaui-MacBookAir.local>
2022-10-23 23:02:05 +0900

    feat: create branch_test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import TodoTemplate from "./components/TodoTemplate/TodoTemplate";
+import BranchTest from "./components/branch_test/BranchTest";
 
 const App = () => {
   return (
     <div className="flex items-center columns-10">
       <TodoTemplate/>
+      <BranchTest/>
     </div>
   );
 }
---
+++ src/components/branch_test/BranchTest.tsx
@@ -0,0 +1,9 @@
+const BranchTest = (): JSX.Element => {
+  return (
+    <div className="text-emerald-400 text-center text-9xl">
+      Branch Test
+    </div>
+  )
+}
+
+export default BranchTest
