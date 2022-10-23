9745abb37e41ac13b8c89e4a5450df40cbd36514
elina <elina@elinaui-MacBookAir.local>
2022-10-23 23:04:54 +0900

    feat: add child_branch

---
+++ src/components/branch_test/ChildBranch.tsx
@@ -0,0 +1,9 @@
+const ChildBranch = (): JSX.Element => {
+  return (
+    <div className="text-emerald-400 text-center text-9xl">
+      Branch Test
+    </div>
+  )
+}
+
+export default ChildBranch
